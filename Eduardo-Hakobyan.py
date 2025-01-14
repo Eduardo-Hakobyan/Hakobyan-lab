543d5dd112aad20f13f38b93f2fb6fc0d68fd0a3
Eduardo-Hakobyan <hakoeduardo258@gmail.com>
2025-01-14 20:43:51 +0100

    Add files via upload

---
+++ Eduardo-Hakobyan.py
@@ -0,0 +1,8 @@
+import turtle
+tartaruga = turtle.Turtle()
+tartaruga.speed("slow")
+for i in range(4): 
+    tartaruga.fillcolor("blue")
+    tartaruga.pencolor("red")
+    tartaruga.right(90)
+    tartaruga.forward(80)
